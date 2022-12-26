6993178fe4b3fbb9c58a65f3bf28253e1a73f682
Barış Atakcı <80262651+rbaris@users.noreply.github.com>
2022-12-26 17:23:58 +0300

    Add files via upload

---
+++ UCI_dermatology/KNNsimulatedAnnealing.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 26 14:35:56 2022
+
+@author: baris
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import cross_val_score
+from sklearn.inspection import partial_dependence,PartialDependenceDisplay,permutation_importance
+import math
+from sklearn.metrics import accuracy_score
+
+
+features ="erythema,scaling,definite borders,itching,koebner phenomenon,polygonal papules,follicular papules,oral mucosal involvement,knee and elbow involvement,scalp involvement,family history,melanin incontinence,eosinophils in the infiltrate,PNL infiltrate,fibrosis of the papillary dermis,exocytosis,acanthosis,hyperkeratosis,parakeratosis,clubbing of the rete ridges,elongation of the rete ridges,thinning of the suprapapillary epidermis,spongiform pustule,munro microabcess,focal hypergranulosis,disappearance of the granular layer,vacuolisation and damage of basal layer,spongiosis,saw-tooth appearance of retes,follicular horn plug,perifollicular parakeratosis,inflammatory monoluclear inflitrate,band-like infiltrate,Age".split(",")
+
+
+filename = "dermatology.csv"
+data = pd.read_csv(filename,sep=',')
+data = data.astype({'Age':'int64','disease':'int64'})
+#print(data.dtypes)
+
+Y = data['disease']
+data = data.drop('disease',axis=1)
+a = data
+
+# Changing column names of x dataframe with 0 and 1
+c = np.random.random(34) > 0.5
+c = c.astype(int).astype(str)
+
+a.columns = c
+
+
+# Taking only 1's of feature for knn
+X = a.loc[:,["1"]]
+
+#train test split
+x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size = 0.3,random_state=True)
+modelKNN = KNeighborsClassifier(n_neighbors=3,metric="euclidean")
+modelKNN.fit(x_train, y_train)
+y_pred = modelKNN.predict(x_test)
+acc = accuracy_score(y_test, y_pred)
+print("KNN için isabet oranı: ",acc)
+
+
+def get_neighbors(a):
+    c = np.random.random(34) > 0.5
+    c = c.astype(int).astype(str)
+    a.columns = c
+    a = a.xs('1', axis=1)
+    return a
+    
+# neigbor parameter is created by get_neighbor so it has randomly selected 1's
+# y is target to predicited
+def get_cost(x,y):
+    
+    x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size = 0.3)
+    modelKNN = KNeighborsClassifier(n_neighbors=3,metric="euclidean")
+    modelKNN.fit(x_train, y_train)
+    y_pred = modelKNN.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    return acc
+
+
+# SA
+# param = dataframe a with 0's and 1's, initial_state is knn score with df a (only 1's selected)
+def simulated_annealing(param, initial_state, Y):
+    """Peforms simulated annealing to find a solution"""
+    
+    
+    initial_temp = 100
+    final_temp = 0
+    alpha = 1
+    
+    current_temp = initial_temp
+
+    # Start by initializing the current state with the initial state
+    current_state = initial_state
+    solution = current_state
+    bestSolution = 0;
+    i = 0        
+    while current_temp >=  final_temp:
+        
+        neighbor = get_neighbors(param)
+        # Check if neighbor is best so far
+        cost_diff = get_cost(neighbor,Y)
+        print(f"{i} . iterasyonda isabet oranı :",cost_diff)
+      
+        if cost_diff < math.exp(cost_diff / current_temp):
+            solution = cost_diff
+        # decrement the temperature
+        current_temp = current_temp - alpha
+        i = i+1
+        
+        if solution > bestSolution:
+            bestSolution = solution
+            
+    return bestSolution
+
+
+print("Simulated Annealing(Benzetilmiş Tavlama) Optimizasyonu Sonrası İsabet Oranı :", simulated_annealing(a, acc , Y))
+print("Simulated Annealing Özellik Seçimi:\n",a.columns)
+print("Optimizasyon öncesi isabet oranı:\n",classification_report(y_test,y_pred))
---
+++ UCI_dermatology/dermatology.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 24 14:23:20 2022
+
+@author: baris
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import GaussianNB
+from sklearn.inspection import partial_dependence,PartialDependenceDisplay,permutation_importance
+from sklearn.metrics import accuracy_score
+
+
+# veri setini dataframe çevirip sonrasında optimizasyon yapılmalı.
+
+#df=pd.read_csv("DFdermatology.csv")
+features ="erythema,scaling,definite borders,itching,koebner phenomenon,polygonal papules,follicular papules,oral mucosal involvement,knee and elbow involvement,scalp involvement,family history,melanin incontinence,eosinophils in the infiltrate,PNL infiltrate,fibrosis of the papillary dermis,exocytosis,acanthosis,hyperkeratosis,parakeratosis,clubbing of the rete ridges,elongation of the rete ridges,thinning of the suprapapillary epidermis,spongiform pustule,munro microabcess,focal hypergranulosis,disappearance of the granular layer,vacuolisation and damage of basal layer,spongiosis,saw-tooth appearance of retes,follicular horn plug,perifollicular parakeratosis,inflammatory monoluclear inflitrate,band-like infiltrate,Age".split(",")
+
+
+filename = "dermatology.csv"
+data = pd.read_csv(filename,sep=',')
+data = data.astype({'Age':'int64','disease':'int64'})
+#print(data.dtypes)
+dataDescription = data.describe()
+
+#Clean Data
+# df.at[34, 'Age'] = 36
+# df.at[35, 'Age'] = 36
+# df.at[36, 'Age'] = 36
+# df.at[37, 'Age'] = 36
+# df.at[264, 'Age'] = 36
+# df.at[265, 'Age'] = 36
+# df.at[266, 'Age'] = 36
+# df.at[267, 'Age'] = 36
+#disease = {1= pityriasis rubra pilaris,2=cronic dermatitis,3=pityriasis rosea,
+#...4=lichen planus,5=seboreic dermatitis ,6= psoriasis}
+
+
+Y = data['disease']
+data = data.drop('disease',axis=1)
+X = data
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3,random_state=True)
+
+modelKNN = KNeighborsClassifier(n_neighbors=3,metric="euclidean")
+modelKNN.fit(x_train, y_train)
+modelNB = GaussianNB()
+modelNB.fit(x_train,y_train)
+modelDT = tree.DecisionTreeClassifier()
+modelDT.fit(x_train,y_train) 
+
+testsayisi = len(x_test)
+print("Test veri sayısı: %d" %testsayisi)
+
+#tahmin yap
+tahminKNN = modelKNN.predict(x_test)
+tahminNB = modelNB.predict(x_test)
+tahminDT = modelDT.predict(x_test)
+
+print("KNN algoritması")
+print(classification_report(y_test,tahminKNN))
+print("*"*50)
+print("Naive Bayes algoritması")
+print(classification_report(y_test,tahminNB))
+print("*"*50)
+print("Karar Ağacı algoritması")
+print(classification_report(y_test,tahminDT))
+#3 a=tree.plot_tree(modelDT,impurity=(False),filled=(True),feature_names=features,class_names=(1))
+#plt.show(a)
+print(tree.export_text(modelDT,feature_names=features))
+print("*"*50)
+
+cmKNN = confusion_matrix(tahminKNN, y_test)
+cmNB = confusion_matrix(tahminNB, y_test)
+cmDT = confusion_matrix(tahminDT, y_test)
+
+
+def objective_functionKNN(solution):
+    if(sum(solution) == 0):
+        return 0
+    model = KNeighborsClassifier(n_neighbors=3)
+    model.fit(x_train.loc[:,solution],y_train)
+    score = model.score(x_test.loc[:,solution], y_test)
+    #print(solution)
+    #print(score)
+    return score
+
+best_objectiveKNN = 0;
+
+for i in range(100):
+    solution = np.random.random(34)>0.5
+    sol_objectiveKNN = objective_functionKNN(solution)    
+    if(sol_objectiveKNN>best_objectiveKNN):
+        best_solution = solution.copy()
+        best_objectiveKNN = sol_objectiveKNN
+print("KNN Optimizasyon")
+best_solution = best_solution.astype(int).astype(str)
+print("ÖZelliklerin Hedef Sınıfa Etkisi:\n",best_solution)
+print("Optimizasyon Sonrası İsabet Oranı:\n",best_objectiveKNN)
+
+def objective_functionNB(solution):
+    if(sum(solution) == 0):
+        return 0
+    model = GaussianNB()
+    model.fit(x_train.loc[:,solution],y_train)
+    score = model.score(x_test.loc[:,solution], y_test)
+    #print(solution)
+    #print(score)
+    return score
+
+best_objectiveNB = 0;
+
+for i in range(100):
+    solution = np.random.random(34)>0.5
+    sol_objectiveNB= objective_functionNB(solution)    
+    if(sol_objectiveNB>best_objectiveNB):
+        best_solution = solution.copy()
+        best_objectiveNB = sol_objectiveNB
+
+print("Naïve Bayes Optimizasyon")
+best_solution = best_solution.astype(int).astype(str)
+print("ÖZelliklerin Hedef Sınıfa Etkisi:\n",best_solution)
+print("Optimizasyon Sonrası İsabet Oranı:\n",best_objectiveNB)
+
+def objective_functionDT(solution):
+    if(sum(solution) == 0):
+        return 0
+    model = tree.DecisionTreeClassifier()
+    model.fit(x_train.loc[:,solution],y_train)
+    score = model.score(x_test.loc[:,solution], y_test)
+    #print(solution)
+    #print(score)
+    return score
+
+best_objectiveDT = 0;
+
+for i in range(100):
+    solution = np.random.random(34)>0.5
+    sol_objectiveDT= objective_functionDT(solution)    
+    if(sol_objectiveDT>best_objectiveDT):
+        best_solution = solution.copy()
+        best_objectiveDT = sol_objectiveDT
+
+print("Karar Ağacı Optimizasyon")
+best_solution = best_solution.astype(int).astype(str)
+print("ÖZelliklerin Hedef Sınıfa Etkisi:\n",best_solution)
+print("Optimizasyon Sonrası İsabet Oranı:\n",best_objectiveDT)
+
+
